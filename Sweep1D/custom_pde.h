c5c7afc871e61f7dbad7e7ede12c92e8d1e7b3ec
W. Beck Andrews <wband@umich.edu>
2026-07-07 18:12:08 -0400

    seemingly working 1D sweep code

---
+++ Sweep1D/custom_pde.h
@@ -0,0 +1,286 @@
+// SPDX-FileCopyrightText: © 2025 Xander Mensah
+// SPDX-License-Identifier: GNU Lesser General Public Version 2.1
+
+#include <prismspf/core/pde_operator_base.h>
+
+#include <complex.h>
+#include <conversion.h>
+
+#include <dual.h>
+#include <variation.h>
+
+#include <ni-cr.h>
+
+PRISMS_PF_BEGIN_NAMESPACE
+
+template <unsigned int dim, unsigned int degree, typename number>
+class CustomPDE : public PDEOperatorBase<dim, degree, number>
+{
+public:
+  using ScalarValue     = dealii::VectorizedArray<number>;
+  using ScalarGrad      = dealii::Tensor<1, dim, ScalarValue>;
+  using ScalarHess      = dealii::Tensor<2, dim, ScalarValue>;
+  using ScalarField     = Dual<ScalarValue, dim>;
+  using ScalarVariation = Variation<ScalarValue, dim>;
+
+  using VectorValue     = dealii::Tensor<1, dim, ScalarValue>;
+  using VectorGrad      = dealii::Tensor<2, dim, ScalarValue>;
+  using VectorHess      = dealii::Tensor<3, dim, ScalarValue>;
+  using VectorField     = Dual<VectorValue, dim>;
+  using VectorVariation = Variation<VectorValue, dim>;
+
+  using ComplexValue     = Complex<ScalarValue>;
+  using ComplexGrad      = dealii::Tensor<1, dim, ComplexValue>;
+  using ComplexHess      = dealii::Tensor<2, dim, ComplexValue>;
+  using ComplexField     = Dual<ComplexValue, dim>;
+  using ComplexVariation = Variation<ComplexValue, dim>;
+
+  using PDEOperatorBase<dim, degree, number>::get_user_inputs;
+  using PDEOperatorBase<dim, degree, number>::get_pf_tools;
+
+  // Hard coded
+  number RT     = 1023.15 * 8.314; // J
+  number Vmfact = 7.0e3;           // number of m*m*nm sheets per mol
+  number gamma  = 2.0;             // J/m^2
+
+  // Parameter file
+  number deltaG0;
+  number D1;
+  number D1s;
+  number D2;
+  number j0;
+  number l_int;
+  number x1_init;
+  number x2_init;
+  number epsilon; // small number to avoid division by zero
+
+  // NiCr Thermo
+  NiCrThermo::Isothermal nicr_energy;
+
+  /**
+   * @brief Constructor.
+   */
+  explicit CustomPDE(const UserInputParameters<dim> &_user_inputs, PhaseFieldTools<dim> &_pf_tools)
+      : PDEOperatorBase<dim, degree, number>(_user_inputs, _pf_tools),
+        deltaG0(get_user_inputs().user_constants.get_double("deltaG0")),
+        D1(get_user_inputs().user_constants.get_double("D1")),
+        D1s(get_user_inputs().user_constants.get_double("D1s")),
+        D2(get_user_inputs().user_constants.get_double("D2")),
+        j0(get_user_inputs().user_constants.get_double("j0")),
+        l_int(get_user_inputs().user_constants.get_double("l_int")),
+        x1_init(get_user_inputs().user_constants.get_double("x1_init")),
+        x2_init(get_user_inputs().user_constants.get_double("x2_init")),
+        epsilon(get_user_inputs().user_constants.get_double("epsilon"))
+  {
+    nicr_energy.set_temperature(RT / NiCrThermo::R);
+  }
+
+private:
+  void
+  set_initial_condition([[maybe_unused]] const unsigned int       &index,
+                        [[maybe_unused]] const unsigned int       &component,
+                        [[maybe_unused]] const dealii::Point<dim> &point,
+                        [[maybe_unused]] number                   &scalar_value,
+                        [[maybe_unused]] number &vector_component_value) const override
+  {
+    // Custom coordinate system
+    const dealii::Tensor<1, dim> &mesh_size =
+        get_user_inputs().spatial_discretization.rectangular_mesh.size;
+    const dealii::Point<dim>      center(mesh_size / 2.0);
+    const dealii::Point<dim>      p(point);
+    [[maybe_unused]] const double x  = (dim > 0) ? p[0] : 0.;
+    [[maybe_unused]] const double y  = (dim > 1) ? p[1] : 0.;
+    [[maybe_unused]] const double z  = (dim > 2) ? p[2] : 0.;
+    [[maybe_unused]] const double lx = (dim > 0) ? mesh_size[0] : 0.;
+    [[maybe_unused]] const double ly = (dim > 1) ? mesh_size[1] : 0.;
+    [[maybe_unused]] const double lz = (dim > 2) ? mesh_size[2] : 0.;
+    // ===========================================================================
+    // FUNCTION FOR INITIAL CONDITIONS
+    // ===========================================================================
+    using std::cos;
+    using std::max;
+    using std::min;
+    using std::sin;
+    using std::sqrt;
+    using std::tanh;
+
+    if (index == 0)
+      {
+        double dist  = x - lx / 2.0;
+        double phi   = interface(dist);
+        scalar_value = max(min(phi, 1.0 - epsilon), epsilon);
+        return;
+      }
+    if (index == 1)
+      {
+        scalar_value = x1_init;
+        return;
+      }
+    if (index == 2)
+      {
+        scalar_value = x2_init;
+        return;
+      }
+    if (index == 3)
+      {
+        scalar_value = 0.0;
+        return;
+      }
+  }
+  void
+  set_dirichlet([[maybe_unused]] const unsigned int       &index,
+                [[maybe_unused]] const unsigned int       &boundary_id,
+                [[maybe_unused]] const unsigned int       &component,
+                [[maybe_unused]] const dealii::Point<dim> &point,
+                [[maybe_unused]] number                   &scalar_value,
+                [[maybe_unused]] number                   &vector_component_value) const override
+  {
+    scalar_value = x2_init;
+  }
+
+  void
+  compute_rhs([[maybe_unused]] FieldContainer<dim, degree, number> &variable_list,
+              [[maybe_unused]] const SimulationTimer               &sim_timer,
+              [[maybe_unused]] unsigned int                         solve_block_id) const override
+  {
+    constexpr double pi = 3.14159265359;
+    const number     dt = sim_timer.get_timestep();
+    if (solve_block_id == 0) // n, x
+      {
+        const ScalarValue n       = variable_list.template get_value<Scalar, OldOne>(0);
+        const ScalarGrad  n_grad  = variable_list.template get_gradient<Scalar, OldOne>(0);
+        const ScalarValue x1      = variable_list.template get_value<Scalar, OldOne>(1);
+        const ScalarGrad  x1_grad = variable_list.template get_gradient<Scalar, OldOne>(1);
+        const ScalarValue x2      = variable_list.template get_value<Scalar, OldOne>(2);
+        const ScalarGrad  x2_grad = variable_list.template get_gradient<Scalar, OldOne>(2);
+        const ScalarValue rxn     = variable_list.template get_value<Scalar, OldOne>(3);
+
+        // n
+        variable_list.set_value_term(0, n + dt * rxn);
+
+        // x1
+        variable_list.set_value_term(
+            1,
+            x1 + dt * ((D1 + 8.0 / l_int * (1.0 - n) * D1s) * x1_grad * n_grad + rxn) /
+                     n);
+        variable_list.set_gradient_term(
+            1, dt * (-(D1 + 8.0 / l_int * (1.0 - n) * D1s) * x1_grad));
+
+        // x2
+        variable_list.set_value_term(2, x2 + dt * (-D2 * x2_grad * n_grad - rxn) / (1.0 - n));
+        variable_list.set_gradient_term(2, dt * (-D2 * x2_grad));
+      }
+    else if (solve_block_id == 1) // delta G
+      {
+        const ScalarValue n      = variable_list.template get_value<Scalar, Current>(0);
+        const ScalarGrad  n_grad = variable_list.template get_gradient<Scalar, Current>(0);
+        const ScalarValue x1     = variable_list.template get_value<Scalar, Current>(1);
+        const ScalarValue x2     = variable_list.template get_value<Scalar, Current>(2);
+
+        // deltaG
+        const Dual<ScalarValue, 1> x1_dual(x1, dealii::Tensor<1, 1, ScalarValue>({1.0}));
+        const Dual<ScalarValue, 1> x2_dual(x2, dealii::Tensor<1, 1, ScalarValue>({1.0}));
+        const Dual<ScalarValue, 1> Gm_a = Gm_alpha(x1_dual);
+        const Dual<ScalarValue, 1> Gm_b = Gm_beta(x2_dual);
+
+        ScalarValue       mu1_chem = Gm_a.val + (1.0 - x1) * Gm_a.grad[0];
+        ScalarValue       mu2_chem = Gm_b.val + (1.0 - x2) * Gm_b.grad[0];
+        const ScalarValue deltaG_val =
+            (mu2_chem - mu1_chem) / RT + 4.0 * Vmfact * gamma / RT / l_int * (2.0 * n - 1.0);
+
+        // const ScalarValue deltaG_val = std::log(x2 / x1 ) + deltaG0 +
+        //                                4.0 * Vmfact * gamma / RT / l_int * (2.0 * n - 1.0);
+        variable_list.set_value_term(4, deltaG_val);
+        variable_list.set_gradient_term(4, -n_grad * 8.0 * Vmfact * gamma / RT * l_int / (pi * pi));
+      }
+    else if (solve_block_id == 2) // rxn
+      {
+        number      upper(1.0 - epsilon);
+        number      lower(epsilon);
+        ScalarValue n      = variable_list.template get_value<Scalar, Current>(0);
+        ScalarGrad  n_grad = variable_list.template get_gradient<Scalar, Current>(0);
+        ScalarValue deltaG = variable_list.template get_value<Scalar, Current>(4);
+
+        ScalarValue rxn_val = -n_grad.norm_square() * n * (1.0 - n) *
+                              (128.0 * l_int / (pi * pi) / 3.0) * Vmfact * j0 * (-deltaG);
+        // alternative 'localization functions' with worse time-step restrictions
+        //     ScalarValue rxn_val = -n_grad.norm() * Vmj0 * (-deltaG);
+        //     ScalarValue rxn_val = -n_grad.norm_square() * (8.0 * l_int/(pi*pi)) * Vmj0 *
+        //     (-deltaG);
+        constrain_dvaldt(n, rxn_val, dt, lower, upper);
+        variable_list.set_value_term(3, rxn_val);
+      }
+    else if (solve_block_id == 3) // pp
+      {
+        const ScalarValue n  = variable_list.template get_value<Scalar, Current>(0);
+        const ScalarValue x1 = variable_list.template get_value<Scalar, Current>(1);
+        const ScalarValue x2 = variable_list.template get_value<Scalar, Current>(2);
+        variable_list.set_value_term(5, n * x1 + (1.0 - n) * x2);
+      }
+  }
+
+  template <typename real>
+  real
+  Gm_alpha(const real &xB) const
+  {
+    // return nicr_energy.G_fcc(xB);
+    using std::log;
+    real xA = 1.0 - xB;
+    real G(0.0);
+    G += RT * (xB * log(xB) + xA * log(xA));
+    return G;
+  }
+
+  template <typename real>
+  real
+  Gm_beta(const real &xB) const
+  {
+    using std::log;
+    real xA = 1.0 - xB;
+    real G(-deltaG0 * RT);
+    G += RT * (xB * log(xB) + xA * log(xA));
+    return G;
+  }
+
+  void
+  pre_solve_block([[maybe_unused]] SolveContext<dim, degree, number> &solve_context,
+                  [[maybe_unused]] unsigned int                       solver_id) override
+  {
+    // solve_context.get_simulation_timer().get_increment();
+    // solve_context.get_user_inputs().spatial_discretization.has_adaptivity;
+  }
+
+  void
+  post_solve_block([[maybe_unused]] SolveContext<dim, degree, number> &solve_context,
+                   [[maybe_unused]] unsigned int                       solver_id) override
+  {}
+
+private:
+  template <typename num>
+  void
+  constrain_dvaldt(const num &val, num &dvaldt, double dt, double lower = 0.0,
+                   double upper = 1.0) const
+  {
+    using std::max;
+    using std::min;
+    num top = max(val + dvaldt * dt, num(upper));
+    num bot = min(val + dvaldt * dt, num(lower));
+    dvaldt  = (dvaldt * dt + (upper - top - (bot - lower))) / dt;
+  }
+
+  /**
+   *@brief return the double obstacle interface function
+   */
+  template <typename real>
+  const real
+  interface(const real &x) const
+  {
+    using std::max;
+    using std::min;
+    using std::sin;
+    constexpr double pi = 3.14159265359;
+    return 0.5 * (1.0 + sin(pi * max(-0.5, min(0.5, x / l_int))));
+  }
+};
+
+PRISMS_PF_END_NAMESPACE
---
+++ Sweep1D/sweep.py
@@ -0,0 +1,144 @@
+import csv
+import itertools
+import os
+import sys
+import subprocess
+
+# --- Configuration ---
+D2_vals = [1.0, 10.0, 100.0]
+deltaG0_vals = [0.0, 0.5, 1.0, 2.0]
+x1_vals = [0.2, 0.6]
+x2_vals = [0.2, 0.05, 0.01]
+l_int_vals = [4.0, 2.0, 1.0, 0.5, 0.25]  # <--- Add your l_int values here
+
+INITIAL_DT = 2.048e-4
+MIN_DT = 1e-8
+DT_REDUCTION_FACTOR = 0.5
+
+TEMPLATE_PRM = "base_parameters.prm"
+LOG_FILE = "sweep_results.csv"
+BASE_OUTPUT_DIR = "sim_outputs"  # All simulation folders will go here
+SIM_COMMAND = "build/main"   # Your Deal.II binary
+
+def generate_prm_file(template_path, output_path,
+                    D2, deltaG0, x1, x2, l_int, dt):
+    Lx = l_int*128.0/10.0
+    Ly = l_int*2.0/10.0
+    with open(template_path, 'r') as f:
+        content = f.read()
+
+    content = content.replace("__LX__", str(Lx))
+    content = content.replace("__LY__", str(Ly))
+    content = content.replace("__DT__", str(dt))
+    content = content.replace("__DELTAG0__", str(deltaG0))
+    content = content.replace("__D2__", str(D2))
+    content = content.replace("__L_INT__", str(l_int))
+    content = content.replace("__X1__", str(x1))
+    content = content.replace("__X2__", str(x2))
+
+    with open(output_path, 'w') as f:
+        f.write(content)
+
+def run_simulation(D2, deltaG0, x1, x2, l_int, dt, run_id, attempt):
+    # 1. Create the unique nested attempt directory
+    run_dir_name = f"run_D{D2}_dG{deltaG0}_x1_{x1}_x2_{x2}_lint_{l_int}"
+    attempt_dir = os.path.join(BASE_OUTPUT_DIR, run_dir_name, f"attempt_{attempt}")
+    os.makedirs(attempt_dir, exist_ok=True)
+    
+    # 2. Get the ABSOLUTE path to your executable before we switch directories
+    # (Assuming build/main is relative to where you start this python script)
+    exe_path = os.path.abspath("build/main")
+    
+    # 3. Put the parameters.prm right inside the attempt directory
+    prm_path = os.path.join(attempt_dir, "parameters.prm")
+    
+    # We no longer pass attempt_dir to this function since your .prm doesn't need it
+    generate_prm_file(TEMPLATE_PRM, prm_path, D2, deltaG0, x1, x2, l_int, dt)
+    
+    # 4. Construct the command. 
+    # Since we will execute *inside* attempt_dir, the parameter file is just "parameters.prm"
+    cmd = ["mpirun", "-n", "1", exe_path, "-i", "parameters.prm"]
+    
+    try:
+        # CRITICAL: cwd=attempt_dir forces mpirun and your sim to run inside that folder.
+        # Your simulation will now create its 'solutions/' folder inside attempt_dir/
+        result = subprocess.run(cmd, check=True,
+            capture_output=True, text=True, cwd=attempt_dir)
+        
+        with open(os.path.join(attempt_dir, "stdout.log"), "w") as f:
+            f.write(result.stdout)
+            
+        return True, "Success", attempt_dir
+        
+    except subprocess.CalledProcessError as e:
+        with open(os.path.join(attempt_dir, "crash_stderr.log"), "w") as f:
+            f.write(e.stderr)
+        with open(os.path.join(attempt_dir, "crash_stdout.log"), "w") as f:
+            f.write(e.stdout)
+            
+        return False, f"Crash (Exit code {e.returncode})", attempt_dir
+
+def main():
+    os.makedirs(BASE_OUTPUT_DIR, exist_ok=True)
+    
+    # 1. Generate ALL possible combinations
+    param_combinations = list(itertools.product(D2_vals, deltaG0_vals, x1_vals, x2_vals, l_int_vals))
+    total_sims = len(param_combinations)
+
+    # 2. Parse command-line arguments to split the workload
+    # Usage: python script.py [chunk_index] [total_chunks]
+    # Example for 4 shells: 
+    # Shell 1: python script.py 0 4
+    # Shell 2: python script.py 1 4  ...etc
+    if len(sys.argv) == 3:
+        chunk_idx = int(sys.argv[1])
+        total_chunks = int(sys.argv[2])
+        
+        # Split the list into roughly equal segments
+        avg = len(param_combinations) / float(total_chunks)
+        start = int(avg * chunk_idx)
+        end = int(avg * (chunk_idx + 1))
+        
+        my_combinations = param_combinations[start:end]
+        # Unique log file per shell to prevent race conditions
+        my_log_file = f"sweep_results_chunk_{chunk_idx}.csv"
+        print(f"Running chunk {chunk_idx + 1}/{total_chunks}. Handling sims {start} to {end} ({len(my_combinations)} total).")
+    else:
+        # Default fallback: run everything if no args are passed
+        my_combinations = param_combinations
+        my_log_file = LOG_FILE
+        print(f"Running full sweep ({total_sims} total combinations).")
+
+    # 3. Process only this chunk's combinations
+    file_exists = os.path.isfile(my_log_file)
+    with open(my_log_file, mode='a', newline='') as f:
+        writer = csv.writer(f)
+        if not file_exists:
+            writer.writerow(["D2", "deltaG0", "x1", "x2", "l_int", "final_dt", "status", "attempts", "output_directory"])
+
+        for i, (D2, deltaG0, x1, x2, l_int) in enumerate(my_combinations, 1):
+            print(f"\n[Combo {i}/{len(my_combinations)}] Testing: D2={D2}, dG0={deltaG0}, x1={x1}, x2={x2}, l_int={l_int}")
+            
+            dt = INITIAL_DT
+            attempts = 0
+            success = False
+            final_dir = ""
+
+            while not success and dt >= MIN_DT:
+                attempts += 1
+                success, status_msg, attempt_dir = run_simulation(D2, deltaG0, x1, x2, l_int, dt, i, attempts)
+                final_dir = attempt_dir
+                
+                if success:
+                    print(f"  ✅ Success! Saved in: {attempt_dir}")
+                    break
+                else:
+                    print(f"  ❌ Failed (dt={dt}). Retrying...")
+                    dt *= DT_REDUCTION_FACTOR
+
+            writer.writerow([D2, deltaG0, x1, x2, l_int, dt if success else "N/A", status_msg, attempts, final_dir])
+            f.flush()
+
+
+if __name__ == "__main__":
+    main()
